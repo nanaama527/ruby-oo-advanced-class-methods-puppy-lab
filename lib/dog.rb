7584e97f65835ab10fad3984ba746ea70f18e4e8
Nana <72888369+nanaama527@users.noreply.github.com>
2021-01-25 02:29:19 -0500

    commit tree b48f39448c810f9f1462a56c5ce66faca6695558

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        self.all.each do |pup|
+            puts pup.name
+        end
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+end
+    
